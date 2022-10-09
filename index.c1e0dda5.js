86cce637f418dec14234353290deec654be87a31
Yauheni Hirynski <104156877+Yauhiry@users.noreply.github.com>
2022-10-09 22:04:47 +0300

    Add files via upload

---
+++ index.c1e0dda5.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0},loop:!0,autoplay:{delay:5e3,disableOnInteraction:!1}});
+//# sourceMappingURL=index.c1e0dda5.js.map
---
+++ index.e4e8407c.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0},loop:!0,autoplay:{delay:5e3,disableOnInteraction:!1}});
+//# sourceMappingURL=index.e4e8407c.js.map
